8b8f058de031f68016a0c42734168a1124b20a98
Titusrono <tituskrono04@gmail.com>
2025-02-24 15:09:51 +0300

    init the enviroment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule,HeaderComponent,RouterOutlet,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
