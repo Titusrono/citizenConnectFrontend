2b9468b816251a9d568232980c90808e1ae30435
RONO <113999110+Titusrono@users.noreply.github.com>
2025-05-06 14:49:28 +0300

    moderator page change

--- src/app/admin/moderator/moderator.component.ts
+++ src/app/admin/moderator/moderator.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-moderator',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './moderator.component.html',
   styleUrl: './moderator.component.scss'
 })
