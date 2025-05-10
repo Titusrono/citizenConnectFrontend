a9f80c7bb9a16eefb7511905fbffe520eaf3d154
RONO <113999110+Titusrono@users.noreply.github.com>
2025-05-10 13:34:31 +0300

    petition more conent to enhance understandability

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common'; // 👈🏽 Needed for *ngIf, *ngFor, etc.
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router'; // 👈🏽 Needed for routing
+import {  RouterOutlet } from '@angular/router'; // 👈🏽 Needed for routing
 
 @Component({
   selector: 'app-dashboard',
   standalone: true, // 👈🏽 Mark it as standalone
-  imports: [CommonModule, RouterLink,RouterOutlet, RouterLinkActive], // 👈🏽 Import everything needed
+  imports: [CommonModule,RouterOutlet], // 👈🏽 Import everything needed
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'] // 👈🏽 Typo fixed here: styleUrls not styleUrl
 })
